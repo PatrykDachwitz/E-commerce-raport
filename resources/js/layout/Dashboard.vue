c9c5c4dc26ba773f65c61bf328a0b4db599e79f8
Patryk Dachwitz <patrykda99@gmail.com>
2024-05-24 21:49:32 +0200

    add dashboard and report views

--- resources/js/layout/Dashboard.vue
+++ resources/js/layout/Dashboard.vue
@@ -6,7 +6,7 @@ console.log(lang);
 </script>
 
 <template>
-    <nav class="navigation px-3">
+    <nav class="dashboard__navigation navigation px-3">
         <span class="navigation__header">{{ lang['dashboard'] }}</span>
         <ul class="navigation__list ps-2">
             <li class="navigation__subsection my-1">{{ lang['dailyReport'] }}</li>
@@ -19,45 +19,41 @@ console.log(lang);
             <li class="navigation__subsection my-1">{{ lang['users'] }}</li>
         </ul>
     </nav>
-    <section class="dashboard">
-        <div class="report d-flex justify-content-between align-items-stretch p-5">
-            <div class="report__content shadow-sm d-flex">
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
-                <div style="min-width: 100px">test</div>
+    <section class="dashboard__content p-5">
+        <div class="report d-flex">
+            <div class="report__container rounded-2 d-flex flex-column">
+                <div class="report__tittle pb-2">
+                    <span>Raport dzienny: 25.12.2014</span>
+                    <div>
+
+                    </div>
+                </div>
+                <div class="report-table d-flex flex-column">
+                    <div class="report-table__row d-flex" v-for="numberElement in 25">
+                        <div class="report-table__col" v-for="numberElement in 25">Kolumna {{  numberElement }}</div>
+                    </div>
+                </div>
             </div>
-            <div class="report__filters shadow-sm ms-5">
-                test
+            <div class="ms-5 report__filters filters rounded-2 overflow-hidden">
+                <div class="report__tittle pb-2">
+                    <span>Filtry</span>
+                    <div>
+
+                    </div>
+                </div>
+
+                <div class="filters__form p-2 px-3">
+                    <template v-for="item in 25">
+
+                        <div class="form-check mb-1 ">
+                            <input class="form-check-input form-check-input--dark" type="checkbox" value="" id="flexCheckChecked" checked>
+                            <label class="form-check-label form-check-label--grey" for="flexCheckChecked">
+                                Udział kosztów
+                            </label>
+                        </div>
+
+                    </template>
+                </div>
             </div>
         </div>
     </section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/assets/logo.ico" type="image/x-icon" >
     <title>{{ config('app.name') }}</title>
 </head>
 <body>
-<div id="app"></div>
+<div id="app" class="dashboard d-flex"></div>
+
 <div style="display: none" data-lang-content>
 @json(__('content'))
 </div>
