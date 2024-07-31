22bfa9dfd31c6141e821ceb29a654ea1a55efe6d
Patryk Dachwitz <patrykda99@gmail.com>
2024-07-31 17:55:37 +0200

    add crona for comand

--- app/Http/Controllers/testGoogle.php
+++ app/Http/Controllers/testGoogle.php
@@ -12,36 +12,6 @@ class testGoogle extends Controller
      */
     public function __invoke(AnalyticsApi $request)
     {
-        $url = "https://oauth2.googleapis.com/token";
-        $response = Http::withHeaders([
-            "Content-Type" => "application/json",
-        ])
-            ->withBody(json_encode($this->buildParams()))
-            ->post($url);
-
-        dd($response->json());
-    }
-    protected function getDataCredentials() {
-        $contentFile = file_get_contents(config('api.pathGoogleAdwordsCredentials'));
-
-        return  json_decode($contentFile)->web;
-    }
-    protected function getClientSecret() : string {
-        return $this->getDataCredentials()
-            ->client_secret;
-    }
-    protected function getClientId() : string {
-        return $this->getDataCredentials()
-            ->client_id;
-    }
-
-    protected function buildParams() {
-        return [
-            "grant_type" => "refresh_token",
-            "client_id" => $this->getClientId(),
-            "client_secret" => $this->getClientSecret(),
-            "refresh_token" => "1//09VoLqiN0LbItCgYIARAAGAkSNwF-L9IrhkX8AhGlfDsCx0a1LqQPgsAZE4DAyxeYgO1oibfZ_kjBGse_yg9cGo7r6oMDxgm6bBs",
-        ];
     }
 
 }
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,22 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+
+$date = date("Y-m-d", strtotime("-1 day"));
+$timezone = "Europe/Warsaw";
+Artisan::command('report:result-day {date}', function (){})
+    ->schedule([$date])
+    ->timezone($timezone)
+    ->dailyAt('6:30');
+
+Artisan::command('report:comparison-day {date}', function (){})
+    ->schedule([$date])
+    ->timezone($timezone)
+    ->dailyAt('6:20');
+
+Artisan::command('report:weekly-day {date}', function (){})
+    ->schedule([$date])
+    ->timezone($timezone)
+    ->dailyAt('6:50')
+    ->mondays();
+
